9665da651f35dfd59aed9147752304c831460631
Ryan DeCook <ryandecook@gmail.com>
2017-11-21 10:13:17 -0600

    add database queries

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE shoes (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80),
+	cost INTEGER
+);
+
+INSERT INTO shoes (name, cost)
+VALUES ('Nike Air Pegasus', 65),
+('Red Wing', 250),
+('Saucony', 40);
+
+SELECT * FROM shoes;
