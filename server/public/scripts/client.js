5659ca5fcafc318b6b9c994ef8876a3e493f76e0
Ryan DeCook <ryandecook@gmail.com>
2017-11-22 13:49:15 -0600

    added put and name change functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,45 @@ console.log('client JS has been loaded');
 
 $(document).ready(function(){
     console.log('JQ loaded');
+    $('#addShoes').on('click', addShoes);
+    getShoes();        
+    $('#shoeList').on('click', '.saveButton', editShoe);
+    $('#shoeList').on('click', '.deleteButton', removeShoe);    
+});//end document ready
+
+function getShoes(){
+    console.log('in getShoes');
+    $.ajax({
+        url: '/shoes',
+        type: 'GET',
+        success: function (response){
+            console.log('got some shoes', response);
+            $('#shoeList').empty();
+            for (var i = 0; i < response.length; i++){
+                var shoe = response[i];
+                var $newShoeItem = $('<li>' + shoe.name + '</li>');
+
+                var $newShoeName = $('<input class="newShoeName" type="text" placeholder="Enter name">');
+                $newShoeItem.append($newShoeName);
+
+                //create and append the save button
+                var $saveShoeButton = $('<button class="saveButton">Save</button>');
+                $newShoeItem.append($saveShoeButton);                
+                $saveShoeButton.data('id', shoe.id);
+
+                //create and append the delete button
+                var $deleteShoeButton = $('<button class="deleteButton">Delete</button>');
+                $deleteShoeButton.data('id', shoe.id);
+                $newShoeItem.append($deleteShoeButton);
+                
+                //append the new list item to the DOM
+                $('#shoeList').append($newShoeItem);
+            }
+        }
+    })
+}
+
+function addShoes(){
     $.ajax({  //including this in document ready is not a common practice, just for this demo
         method: 'POST',
         url: '/shoes',
@@ -11,7 +50,40 @@ $(document).ready(function(){
         },
         success: function(response){
             console.log('response', response);
+            getShoes();
             
         }
     })
-});
+}
+
+function removeShoe(){
+    console.log($(this).data());
+    var shoeIdToRemove = $(this).data().id;
+    console.log('remove shoe was clicked! The shoe id was ', shoeIdToRemove);
+
+    $.ajax ({
+        method: 'DELETE',
+        url: '/shoes/' + shoeIdToRemove,
+        success: function(response){
+            getShoes();
+        }
+    })
+}
+
+function editShoe(){
+    console.log($(this).data()); // this should love {id:7} or whatever id is  
+    var shoeIdToSave = $(this).data().id;
+    var shoeNameToSave = $(this).parent().children(".newShoeName").val();
+    console.log('save shoe was clicked! The shoe id was ', shoeIdToSave);
+
+    $.ajax({
+        method: 'PUT',
+        url: '/shoes/' + shoeIdToSave,
+        data: {
+            name: shoeNameToSave,
+        },
+        success: function (response) {
+            getShoes();
+        }
+    })
+}
--- server/routes/shoes.js
+++ server/routes/shoes.js
@@ -14,7 +14,7 @@ router.get('/', function(req,res){
         } else {
             // We connected to the database!
             //Now, we're going to GET things from the DB
-            client.query('SELECT * FROM shoes;', function(errorMakingQuery, result){
+            client.query('SELECT * FROM shoes ORDER BY id;', function(errorMakingQuery, result){
                 done();
                 if(errorMakingQuery){
                     //Query failed.  Did you test it in Postico?
@@ -50,6 +50,61 @@ router.post('/', function (req,res){
                     res.sendStatus(500);
                 } else{
                     res.sendStatus(201); //201 is status 'created'
+                }
+            });
+        }
+    });
+})
+
+router.delete('/:id', function (req,res){
+    var shoeIdToRemove = req.params.id;
+    //Attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            //There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!
+            //Now, we're going to GET things from the DB
+            //use ES6 backtick ` to select multi lines below****
+            client.query(`DELETE FROM shoes WHERE id=$1;`, [shoeIdToRemove], function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    //Query failed.  Did you test it in Postico?
+                    //Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else{
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+})
+
+router.put('/:id', function (req,res){
+    var shoeIdToSave = req.params.id;
+    var shoeNameToSave = req.body.name;
+    //Attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            //There was an error connecting to the database
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        } else {
+            // We connected to the database!
+            //Now, we're going to GET things from the DB
+            //use ES6 backtick ` to select multi lines below****
+            client.query(`UPDATE shoes SET "name" = $1 WHERE "id" = $2;`, [shoeNameToSave, shoeIdToSave], function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    //Query failed.  Did you test it in Postico?
+                    //Log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else{
+                    res.sendStatus(200);
                 }
             });
         }
